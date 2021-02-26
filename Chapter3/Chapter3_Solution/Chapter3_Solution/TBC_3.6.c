05359eaafd82b610741499cc452225aac9180949
greyn051 <64242999+greyn051@users.noreply.github.com>
2021-02-26 19:16:19 +0900

    chapter3.6 example

---
+++ Chapter3/Chapter3_Solution/Chapter3_Solution/TBC_3.6.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main() {
+    unsigned int i = 0;
+
+    printf("%u\n", sizeof(unsigned int));
+    printf("%u", sizeof(i));
+
+    return 0;
+
+}
