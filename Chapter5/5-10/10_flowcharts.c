3d0d313286f51fb54a0046e000194869dba1c7aa
Refi Go <64242999+refigo@users.noreply.github.com>
2021-10-30 11:39:43 +0900

    Add 5-10

---
+++ Chapter5/5-10/10_flowcharts.c
@@ -0,0 +1,18 @@
+/*
+## 5.10 순서도(Flowcharts)
+*/
+
+int main() {
+    int i = 0;
+    while (i < 10)
+    // an example of compound statements
+    // (block)
+    {
+    i++;
+    printf("%d\n", i);
+    }
+
+    return 0;
+}
+
+// { }를 compound statements나 block으로 부름
